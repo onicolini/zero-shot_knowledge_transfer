9dea226ab4726e8e746d9c329ef0bd9cfdbf9c50
= <simone.zamboni996@gmail.com>
2019-10-03 10:53:34 +0000

    added our keras teachers

---
+++ our_code/teacher-40-1.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import numpy as np
+import sklearn.metrics as metrics
+
+from keras.models import Model
+from keras.layers import Input, Add, Activation, Dropout, Flatten, Dense
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras import backend as K
+
+#import wide_residual_network as wrn
+from keras.datasets import cifar10
+import keras.callbacks as callbacks
+import keras.utils.np_utils as kutils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import plot_model
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.datasets import cifar10
+from keras.callbacks import LearningRateScheduler
+import numpy as np
+from keras import optimizers
+
+
+
+weight_decay = 0.0005
+
+def initial_conv(input):
+    x = Convolution2D(16, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(input)
+
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+    return x
+
+
+def expand_conv(init, base, k, strides=(1, 1)):
+    x = Convolution2D(base * k, (3, 3), padding='same', strides=strides, kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(init)
+
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(base * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    skip = Convolution2D(base * k, (1, 1), padding='same', strides=strides, kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(init)
+
+    m = Add()([x, skip])
+
+    return m
+
+
+def conv1_block(input, k=1, dropout=0.0):
+    init = input
+
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(input)
+    x = Activation('relu')(x)
+    x = Convolution2D(16 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    if dropout > 0.0: x = Dropout(dropout)(x)
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+    x = Convolution2D(16 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    m = Add()([init, x])
+    return m
+
+def conv2_block(input, k=1, dropout=0.0):
+    init = input
+
+    #channel_axis = 1 if K.image_dim_ordering() == "th" else -1
+    channel_axis = -1
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(input)
+    x = Activation('relu')(x)
+    x = Convolution2D(32 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    if dropout > 0.0: x = Dropout(dropout)(x)
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+    x = Convolution2D(32 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    m = Add()([init, x])
+    return m
+
+def conv3_block(input, k=1, dropout=0.0):
+    init = input
+
+    #channel_axis = 1 if K.image_dim_ordering() == "th" else -1
+    channel_axis = -1
+    
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(input)
+    x = Activation('relu')(x)
+    x = Convolution2D(64 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    if dropout > 0.0: x = Dropout(dropout)(x)
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+    x = Convolution2D(64 * k, (3, 3), padding='same', kernel_initializer='he_normal',
+                      W_regularizer=l2(weight_decay),
+                      use_bias=False)(x)
+
+    m = Add()([init, x])
+    return m
+
+def create_wide_residual_network(input_dim, nb_classes=100, N=2, k=1, dropout=0.0, verbose=1):
+    """
+    Creates a Wide Residual Network with specified parameters
+
+    :param input: Input Keras object
+    :param nb_classes: Number of output classes
+    :param N: Depth of the network. Compute N = (n - 4) / 6.
+              Example : For a depth of 16, n = 16, N = (16 - 4) / 6 = 2
+              Example2: For a depth of 28, n = 28, N = (28 - 4) / 6 = 4
+              Example3: For a depth of 40, n = 40, N = (40 - 4) / 6 = 6
+    :param k: Width of the network.
+    :param dropout: Adds dropout if value is greater than 0.0
+    :param verbose: Debug info to describe created WRN
+    :return:
+    """
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    ip = Input(shape=input_dim)
+
+    x = initial_conv(ip)
+    nb_conv = 4
+
+    x = expand_conv(x, 16, k)
+    nb_conv += 2
+
+    for i in range(N - 1):
+        x = conv1_block(x, k, dropout)
+        nb_conv += 2
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+
+    x = expand_conv(x, 32, k, strides=(2, 2))
+    nb_conv += 2
+
+    for i in range(N - 1):
+        x = conv2_block(x, k, dropout)
+        nb_conv += 2
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+
+    x = expand_conv(x, 64, k, strides=(2, 2))
+    nb_conv += 2
+
+    for i in range(N - 1):
+        x = conv3_block(x, k, dropout)
+        nb_conv += 2
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+
+    x = AveragePooling2D((8, 8))(x)
+    x = Flatten()(x)
+
+    x = Dense(nb_classes, W_regularizer=l2(weight_decay), activation='softmax')(x)
+
+    model = Model(ip, x)
+
+    if verbose: print("Wide Residual Network-%d-%d created." % (nb_conv, k))
+    return model
+
+def lr_schedule(epoch):
+    lrate = 0.1
+    if epoch > 60:
+        lrate = 0.02
+    if epoch > 120:
+        lrate = 0.004
+    if epoch > 160:
+        lrate = 0.0008
+    return lrate
+
+batch_size = 128
+num_classes = 10
+epochs = 200
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
+    input_shape = (3, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+#old normalization
+#x_train /= 255
+#x_test /= 255
+
+# new normalization with z-score
+mean = np.mean(x_train,axis=(0,1,2,3))
+std = np.std(x_train,axis=(0,1,2,3))
+x_train = (x_train-mean)/(std+1e-7)
+x_test = (x_test-mean)/(std+1e-7)
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+#model definition
+# For WRN-16-8 put N = 2, k = 8
+# For WRN-28-10 put N = 4, k = 10
+# For WRN-40-4 put N = 6, k = 4
+model = create_wide_residual_network(input_shape, nb_classes=10, N=6, k=1, dropout=0.00)
+
+model.summary()
+plot_model(model, "WRN-16-2.png", show_shapes=False)
+
+# define optimizer
+sgd = optimizers.SGD(lr=0.1, decay=5e-4, momentum=0.9)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=2,
+          validation_data=(x_test, y_test),
+         callbacks=[LearningRateScheduler(lr_schedule)])
+
+#save to disk
+model_json = model.to_json()
+with open('model-40-1.json', 'w') as json_file:
+    json_file.write(model_json)
+model.save_weights('model-40-1.h5') 
+
+# final evaluation on test
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
